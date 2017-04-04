055072adb7b970ce69451d15de1244de4880e124
scienceopen <scienceopen@users.noreply.github.com>
2017-04-04 16:51:08 -0400

    prreq

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
-    import pip
-    pip.main(['install','-r','requirements.txt'])
+
+req = ['nose','pillow','scipy','numpy','matplotlib']
 
 setup(name='pyoptflow',
+      packages=['pyoptflow'],
       author='Michael Hirsch, Ph.D.',
-      url='https://github.com/scienceopen/pyoptflow',
+      url='https://github.com/scivision/pyoptflow',
       version='0.5',
       classifiers=[
       'Topic :: Scientific/Engineering :: Image Analysis',
       'Development Status :: 3 - Alpha',
-      'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
       ],
       description='Pure Python optical flow: Horn-Schunck, Lucas-Kanade',
-      packages=['pyoptflow'],
+      install_requires=req,
 	  )
