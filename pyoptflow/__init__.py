44d5352b49e4c68b1e97705972bc463088a48bea
scienceopen <scienceopen@users.noreply.github.com>
2017-04-05 02:59:23 -0400

    better defaults

--- pyoptflow/__init__.py
+++ pyoptflow/__init__.py
@@ -4,13 +4,25 @@ from scipy.ndimage.filters import convolve as filter2
 #
 from .plots import plotderiv
 
-def HornSchunck(im1, im2, alpha=1., Niter=100, verbose=False):
+HSKERN =np.array([[1/12, 1/6, 1/12],
+                  [1/6,    0, 1/6],
+                  [1/12, 1/6, 1/12]],float)
+
+kernelX = np.array([[-1, 1],
+                     [-1, 1]]) * .25 #kernel for computing d/dx
+kernelY = np.array([[-1,-1],
+                     [ 1, 1]]) * .25 #kernel for computing d/dy
+kernelT = np.ones((2,2))*.25
+
+def HornSchunck(im1, im2, alpha=0.001, Niter=8, verbose=False):
     """
     im1: image at t=0
     im2: image at t=1
     alpha: regularization constant
     Niter: number of iteration
     """
+    im1 = im1.astype(np.float32)
+    im2 = im2.astype(np.float32)
 
 	#set up initial velocities
     uInitial = np.zeros([im1.shape[0],im1.shape[1]])
@@ -26,18 +38,13 @@ def HornSchunck(im1, im2, alpha=1., Niter=100, verbose=False):
     if verbose:
         plotderiv(fx,fy,ft)
 
-	# Averaging kernel
-    kernel=np.array([[1/12, 1/6, 1/12],
-                      [1/6,    0, 1/6],
-                      [1/12, 1/6, 1/12]],float)
-
-    print(fx[100,100],fy[100,100],ft[100,100])
+#    print(fx[100,100],fy[100,100],ft[100,100])
 
 	# Iteration to reduce error
     for _ in range(Niter):
 #%% Compute local averages of the flow vectors
-        uAvg = filter2(U,kernel)
-        vAvg = filter2(V,kernel)
+        uAvg = filter2(U, HSKERN)
+        vAvg = filter2(V, HSKERN)
 #%% common part of update step
         der = (fx*uAvg + fy*vAvg + ft) / (alpha**2 + fx**2 + fy**2)
 #%% iterative step
@@ -47,17 +54,11 @@ def HornSchunck(im1, im2, alpha=1., Niter=100, verbose=False):
     return U,V
 
 def computeDerivatives(im1, im2):
-#%% build kernels for calculating derivatives
-    kernelX = np.array([[-1, 1],
-                         [-1, 1]]) * .25 #kernel for computing d/dx
-    kernelY = np.array([[-1,-1],
-                         [ 1, 1]]) * .25 #kernel for computing d/dy
-    kernelT = np.ones((2,2))*.25
 
     fx = filter2(im1,kernelX) + filter2(im2,kernelX)
     fy = filter2(im1,kernelY) + filter2(im2,kernelY)
 
-    #ft = im2 - im1
+   # ft = im2 - im1
     ft = filter2(im1,kernelT) + filter2(im2,-kernelT)
 
     return fx,fy,ft
